94eceae88fe5e9afcedd1beed14a5d90e08c61ae
Robertosnap <robertosnap@pm.me>
2022-08-18 14:44:16 +0200

    Added random company

--- packages/demo-frontend2/pages/api/random-company.ts
+++ packages/demo-frontend2/pages/api/random-company.ts
@@ -1,22 +1,34 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { readFileSync } from "fs";
-import {join, resolve} from 'path'
-import getConfig from 'next/config';
+import {resolve} from 'path'
+import { Organisasjon } from './demo-data/companies.types';
 
 
 type Data = {
-  length: number
+  data: Organisasjon[]
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const serverRuntimeConfig= getConfig()
-  const file = resolve( './pages/api/demo-data/companies.json');
-  const json = readFileSync( file, "utf8");
-  const companies = JSON.parse(json ) as any[]
-  companies.length
-  res.status(200).json({ length: companies.length })
+  if (req.method === 'GET') {
+    if("amount" in req.query && typeof req.query.amount === "string") {
+      const amount = parseInt(req.query.amount)
+      const file = resolve( './pages/api/demo-data/companies.json');
+      const json = readFileSync( file, "utf8");
+      const data = JSON.parse(json ) as Organisasjon[]
+      let orgs : Organisasjon[]= [];
+      for(let i = 0; i < amount; i++) {
+        const random = Math.floor(Math.random() * (data.length + 1) + 0)
+        console.log(random)
+        orgs.push(data[random])
+      }
+      res.status(200).json({
+        data: orgs
+      })
+    }
+  }
+  res.status(500)
 }
--- packages/demo-frontend2/pages/publish.tsx
+++ packages/demo-frontend2/pages/publish.tsx
@@ -5,40 +5,56 @@ import { useRouter } from 'next/router';
 import { Plus, Search } from 'react-iconly';
 import { Footer } from '../src/ui/Footer';
 import { NavBar } from '../src/ui/NavBar';
+import { SDK } from "@brok/sdk2";
+import useSWR from "swr";
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const Home: NextPage = () => {
   const router = useRouter()
 
+  const getRandomOrgs = async (amount: number) => {
+    const company = await fetch(`/api/random-company?${new URLSearchParams({
+      amount: amount.toString(),
+    })}`)
+    const json = await company.json()
+    if ("data" in json) {
+      return json.data
+    }
+    throw "No data in response"
+  }
+
   const simulate = async () => {
+    console.log(await getRandomOrgs(2))
+
     // const sdk = await SDK.init({ ceramicUrl: "https://ceramic-clay.3boxlabs.com", ethereumRpc: "http://127.0.0.1:8545/", secret: "kid letter bicycle motion maid token change couch useless seven boost strategy", theGraphUrl: "http://localhost:8000/subgraphs/name/brok/captable", env: "brokLocal" });
     // const address = await sdk.createCapTable({
-    //     name: "Test 535325",
-    //     orgnr: "41412124",
-    //     shareholders: [
-    //         {
-    //             amount: "6664444",
-    //             birthDate: "230188",
-    //             countryCode: "NO",
-    //             name: "TES PERSON",
-    //             partition: "ordinære",
-    //             postalcode: "50553"
-    //         }
-    //     ]
+    //   name: "Test 535325",
+    //   orgnr: "41412124",
+    //   shareholders: [
+    //     {
+    //       amount: "6664444",
+    //       birthDate: "230188",
+    //       countryCode: "NO",
+    //       name: "TES PERSON",
+    //       partition: "ordinære",
+    //       postalcode: "50553"
+    //     }
+    //   ]
     // })
     // console.log("CapTableAddress", address)
     // const capTable = await sdk.getCapTable(address)
     // console.log("capTable", capTable)
     // const transferResult = await sdk.transfer(address, [
-    //     {
-    //         amount: '100',
-    //         partition: 'ordinære',
-    //         from: capTable.shareholders[0].ethAddress,
-    //         name: "Jon",
-    //         birthDate: "202020",
-    //         countryCode: "NO",
-    //         postalcode: "0655",
+    //   {
+    //     amount: '100',
+    //     partition: 'ordinære',
+    //     from: capTable.shareholders[0].ethAddress,
+    //     name: "Jon",
+    //     birthDate: "202020",
+    //     countryCode: "NO",
+    //     postalcode: "0655",
 
-    //     },
+    //   },
     // ]);
     // console.log("transferResult", transferResult)
   }
@@ -62,7 +78,7 @@ const Home: NextPage = () => {
       >
         <Spacer y={4}></Spacer>
         <Grid.Container gap={2} css={{ p: '$sm' }} justify="center" >
-          <Button>Start simulation</Button>
+          <Button onPress={() => simulate()}>Start simulation</Button>
         </Grid.Container>
 
       </Container>
